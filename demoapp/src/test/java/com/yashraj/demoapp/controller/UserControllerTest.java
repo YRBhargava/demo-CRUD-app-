9a22e54c9d74303210f69e0b27abca29a48d79c1
Yashraj Bhargava <yashrajbhargava@Yashrajs-MacBook-Pro.local>
2024-01-17 07:33:36 -0500

    used context api

---
+++ demoapp/src/test/java/com/yashraj/demoapp/controller/UserControllerTest.java
@@ -0,0 +1,48 @@
+package com.yashraj.demoapp.controller;
+
+import com.yashraj.demoapp.model.User;
+import com.yashraj.demoapp.repository.UserRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserControllerTest {
+
+    @Autowired
+    private UserController userController;
+    @Mock
+    private UserRepository userRepository;
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void newUser() {
+        userController.getAllUsers();
+    }
+
+    @Test
+    void getAllUsers() {
+    }
+
+    @Test
+    void getUserById() {
+    }
+
+    @Test
+    void updateUser() {
+    }
+
+    @Test
+    void deleteUser() {
+    }
+}
